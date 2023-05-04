561e240eb55da3fdd6ad17fe480128ec9b399971
Nelson Garcia <ngarcia@econtainers.co>
2023-05-04 16:56:41 -0500

    init

---
+++ asteroids_nasa/lib/presentation/screens/sing_in/authentication.dart
@@ -0,0 +1,18 @@
+import 'package:asteroids_nasa/presentation/providers/firebase_provider.dart';
+import 'package:flutter/material.dart';
+
+class Authentication extends StatefulWidget {
+  const Authentication({super.key});
+
+  @override
+  State<Authentication> createState() => _AuthenticationState();
+}
+
+class _AuthenticationState extends State<Authentication> {
+  late FirebaseProvider firebaseProvider;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
